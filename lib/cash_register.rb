3ebcfa2f2c0d799619fe8cc66c481999e08627c7
Laura Berge <lauraeberge@gmail.com>
2019-09-11 13:07:03 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,13 +1,14 @@
 class CashRegister
   attr_accessor :total, :cash_register, :discount
   
-  def cash_register
-    @cash_register = self.new
+  def initialize
+    #@cash_register = self.new
     @total = 0
   end
   
   def cash_register_with_discount(n)
-    cash_register
+    @cash_register = self.new
+    @total = 0
     @discount = 20
   end
   
